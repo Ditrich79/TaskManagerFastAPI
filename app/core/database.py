1629b471b97db9bb09ba33115ce8f17e34304826
Ditrich79 <adk18@mail.ru>
2026-06-28 17:43:33 +0300

    Fixed database

--- app/core/database.py
+++ app/core/database.py
@@ -1,27 +1,24 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 from typing import Annotated
 
 # Создаем асинхронный движок для PostgreSQL
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=True, pool_pre_ping=True)
 
 # Создаем фабрику сессий. Каждый запрос будет получать свою сессию для работы с БД.
 AsyncSessionLocal = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
+    bind=engine, class_=AsyncSession, expire_on_commit=False
 )
 
 # Базовый класс для наших моделей SQLAlchemy
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 # Зависимость FastAPI, которая будет предоставлять сессию БД для каждого запроса
 async def get_db():
     async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
-
+        yield session
 
 GetAsyncSession = Annotated[AsyncSession, Depends(get_db)]
